b4bfdfafc8a166c13b4e4959cd85c751880a130e
miguelmdaguiar <mmaro3@iscte-iul.pt>
2025-10-16 15:23:56 +0200

    Integrate QR Code generation into Application and remove QRCodeTest

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.example.utils.QRCodeGenerator;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +12,8 @@ public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
 
+        QRCodeGenerator.generateQRCode("ToDoApp", "qrcode_app.png");
+        System.out.println("QR Code criado");
+    }
 }
