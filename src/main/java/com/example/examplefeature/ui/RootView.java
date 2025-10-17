ca39ca856f2955995d2950f616b7e4e7a8f3aa25
montezdesousa <montezdesousa@gmail.com>
2025-10-17 14:15:20 +0200

    Update RootView

--- src/main/java/com/example/examplefeature/ui/RootView.java
+++ src/main/java/com/example/examplefeature/ui/RootView.java
@@ -1,4 +1,15 @@
 package com.example.examplefeature.ui;
 
-public class RootView {
+import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class RootView extends Main implements BeforeEnterObserver {
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        event.forwardTo("tasks");
+    }
 }
